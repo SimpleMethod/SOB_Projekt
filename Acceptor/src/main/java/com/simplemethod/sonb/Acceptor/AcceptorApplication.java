00177d5cd32a019fc67621481d91920457adbb53
Piotr N <p.nowacki13@gmail.com>
2021-04-13 19:39:47 +0200

    Added blank spring boot application for acceptor

---
+++ Acceptor/src/main/java/com/simplemethod/sonb/Acceptor/AcceptorApplication.java
@@ -0,0 +1,13 @@
+package com.simplemethod.sonb.Acceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcceptorApplication.class, args);
+	}
+
+}
---
+++ Acceptor/src/test/java/com/simplemethod/sonb/Acceptor/AcceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplemethod.sonb.Acceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
