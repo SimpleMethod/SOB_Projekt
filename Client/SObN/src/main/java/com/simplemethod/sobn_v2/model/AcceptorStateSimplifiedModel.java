710e498fa7563b661f452cbb16c85fb1cd6a755a
Michał Młodawski <michalmpm1122@gamil.com>
2021-05-23 17:57:12 +0200

    Auto stash before rebase of "origin/master"

--- Client/SObN/src/main/java/com/simplemethod/sobn_v2/model/AcceptorStateSimplifiedModel.java
+++
@@ -1,9 +0,0 @@
-package com.simplemethod.sobn_v2.model;
-
-public class AcceptorStateSimplifiedModel {
-
-    private Integer currentError;    //Null = status ok, and no error. Value = status "X", and error
-
-    private Integer currentSequenceNumber = 1;
-
-}
